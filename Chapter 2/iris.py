cb8b6ade338ec2ef40d7c457ee189fc41bef384d
SuperVingo <suvingo@naver.com>
2021-12-22 03:38:26 +0900

    Add source code
    Add source code
    Gradient Descent.ipynb
    Stochastic Gradient Descent.ipynb
    AdalineGD.py
    
    Delete iris.py

--- Chapter 2/iris.py
+++
@@ -1,7 +0,0 @@
-import os
-import pandas as pd
-s = os.path.join("https://archive.ics,uci.edu", "ml", "machine-learning-databases", "iris", "iris.data")
-print('URL : ', s)
-
-df = pd.read_csv(s, header=None, encoding="UTF-8")
-df.tail()
